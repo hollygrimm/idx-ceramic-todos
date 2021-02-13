85ecc500f865f4abc7f8b33fd8c1738ab3ef0ce0
John Thoms <jthoms@sustia.com>
2021-02-13 11:14:06 -0700

    polish

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ceramic-todos'),
+      dir: require('path').join(__dirname, './coverage/idx-ceramic-todos'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ceramic-todos'`, () => {
+  it(`should have as title 'idx-ceramic-todos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ceramic-todos');
+    expect(app.title).toEqual('idx-ceramic-todos');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ceramic-todos app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('idx-ceramic-todos app is running!');
   });
 });
